979cc8c3bb033cdcd46b46209b8eed4847e8fba3
Konrad <kondykox@gmail.com>
2024-02-26 19:38:28 +0100

    translation tools

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,19 @@
 import { useState } from "react";
+import { I18nextProvider, useTranslation } from "react-i18next";
+import i18n from "./i18n";
 import "./App.css";
 
 const App = () => {
+  const { t } = useTranslation();
 
+  return (
+    <I18nextProvider i18n={i18n}>
+      <div>
+        <h1>{t("greeting")}</h1>
+        <p>{t("content")}</p>
+      </div>
+    </I18nextProvider>
+  );
 };
 
 export default App;
